273ebfa11a4d114f284dfe3a09b0a091be6c3567
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-02-10 23:25:28 -0500

    Get user input from the terminal

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes');
 
-// console.log(_.isString(true));
-// console.log(_.isString('andrew'));
-var filteredArray = _.uniq(['yunier', 'yunier', 1, 2, 3, 4]);
-console.log(filteredArray);
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
 
-
-// var res = notes.addNote();
-// var add = notes.add(2, 3);
-// console.log(res);
-// console.log(add);
-
-// var user = os.userInfo();
-// console.log(user);
-// fs.appendFile(
-//     'greetings.txt',
-//     'Hello ' + user.username + '! Your are ' + notes.age + '.\n',
-//     () => {}
-// );
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Fetching note');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+    console.log('Command not recognized');
+}
