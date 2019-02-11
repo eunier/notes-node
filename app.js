79bc99810ca96518b74c21c39f9fb1652c5c235a
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-02-10 22:39:35 -0500

    Add demostration code

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-console.log('starting app');
+console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes');
 
-var user = os.userInfo();
-console.log(user);
-fs.appendFileSync('greetings.txt', 'hello ${user.username}!');
+// console.log(_.isString(true));
+// console.log(_.isString('andrew'));
+var filteredArray = _.uniq(['yunier', 'yunier', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+
+// var res = notes.addNote();
+// var add = notes.add(2, 3);
+// console.log(res);
+// console.log(add);
+
+// var user = os.userInfo();
+// console.log(user);
+// fs.appendFile(
+//     'greetings.txt',
+//     'Hello ' + user.username + '! Your are ' + notes.age + '.\n',
+//     () => {}
+// );
